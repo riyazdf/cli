e335aa8f6be901f9dd4b7b64f7241743863051ad
Ashwini Oruganti <ashwini.oruganti@gmail.com>
2017-07-26 13:34:28 -0700

    Add info flags

--- cli/command/trust/info.go
+++ cli/command/trust/info.go
@@ -6,12 +6,21 @@ import (
 	"github.com/spf13/cobra"
 )
 
+type infoOptions struct {
+	tagsOnly bool
+	keysOnly bool
+}
+
 func newInfoCommand(dockerCli command.Cli) *cobra.Command {
+	var opts infoOptions
 	cmd := &cobra.Command{
 		Use:   "info [OPTIONS]",
 		Short: "Display detailed information about keys and signatures",
 		Args:  cli.NoArgs,
 		RunE:  command.ShowHelp(dockerCli.Err()),
 	}
+	flags := cmd.Flags()
+	flags.BoolVarP(&opts.tagsOnly, "tags-only", "t", false, "Show all signed tags only")
+	flags.BoolVarP(&opts.keysOnly, "keys-only", "k", false, "Show only keys and delegations known to the repo")
 	return cmd
 }
